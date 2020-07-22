b5a4bfda13eb6ac368b039fdaa2d8196da9a048d
NetanelBasal <netanel7799@gmail.com>
2020-07-22 12:39:05 +0300

    init

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,29 +1,47 @@
-import { useMyHook } from './'
+import { useObservable } from './'
 import { renderHook, act } from "@testing-library/react-hooks";
+import { interval, BehaviorSubject } from 'rxjs';
 
 // mock timer using jest
 jest.useFakeTimers();
 
-describe('useMyHook', () => {
-  it('updates every second', () => {
-    const { result } = renderHook(() => useMyHook());
-
-    expect(result.current).toBe(0);
-
-    // Fast-forward 1sec
+describe('useObservable', () => {
+  
+  it('should support BehaviorSubject', () => {
+    const query = new BehaviorSubject('init');
+    const { result } = renderHook(() => useObservable(query));
+    let [next] = result.current;
+    expect(next).toBe('init');
     act(() => {
-      jest.advanceTimersByTime(1000);
+      query.next('2');
     });
 
-    // Check after total 1 sec
-    expect(result.current).toBe(1);
+    [next] = result.current;
+    expect(next).toBe('2');
+  });
 
-    // Fast-forward 1 more sec
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
+  // it('updates every second', () => {
+  //   const { result } = renderHook(() => useObservable(interval(1000), 0));
+  //   let [next] = result.current;
 
-    // Check after total 2 sec
-    expect(result.current).toBe(2);
-  })
+  //   expect(next).toBe(0);
+
+  //   // // Fast-forward 1sec
+  //   act(() => {
+  //     jest.advanceTimersByTime(1000);
+  //   });
+
+  //   [next] = result.current;
+  //   // // Check after total 1 sec
+  //   expect(next).toBe(0);
+
+  //   // // Fast-forward 1 more sec
+  //   act(() => {
+  //     jest.advanceTimersByTime(1000);
+  //   });
+
+  //   [next] = result.current;
+  //   // Check after total 2 sec
+  //   expect(next).toBe(1);
+  // })
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,60 @@
-import * as React from 'react';
+import { Observable, Subscription } from 'rxjs';
+import { useRef, useState, useEffect } from 'react';
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
+export function useObservable<T, E>(source$: Observable<T>, initialValue?: T | undefined): [T, E | undefined, boolean] {
+  const sourceRef$ = useRef<Observable<T>>(source$);
+  const isBehaviorSubject = initialValue === undefined;
+  
+  const [error, setError] = useState<E | undefined>();
+  const [completed, setComplete] = useState<boolean>(false);
+  const [next, setValue] = useState<T>(() => {
+    if(isBehaviorSubject) {
+      let firstValue: T | undefined = undefined;
+
+      let subscription: Subscription | null = sourceRef$.current.subscribe(v => {
+        firstValue = v;
+      });
+
+      subscription.unsubscribe();
+      subscription = null;
+
+      return firstValue! as T;
+    } 
+
+    return initialValue!;
   });
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+  useEffect(() => {
+    let subscription: Subscription;
 
-  return counter;
+    if(isBehaviorSubject) {
+      let firstEmission = true;
+      subscription = sourceRef$.current.subscribe({
+        next(v) {
+            if(!firstEmission) {
+              setValue(v);
+            }
+            firstEmission = false;
+        },
+         error: setError,
+        complete() {
+          setComplete(true)
+        }
+      });
+    } else {
+      subscription = sourceRef$.current.subscribe({
+        next: setValue,
+        error: setError,
+        complete() {
+          setComplete(true)
+        }
+      });
+    }
+
+    return () => {
+      subscription.unsubscribe();
+    }
+  }, [sourceRef$.current]);
+
+  return [next, error, completed];
 };
